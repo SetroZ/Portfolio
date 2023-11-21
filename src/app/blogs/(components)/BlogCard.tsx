8ee0ec9fc2d9638880440aee161a87b59be8df1d
SetroZ <yousfsbry66@gmail.com>
2023-11-21 11:15:33 +0800

    fixed errors

--- src/app/blogs/(components)/BlogCard.tsx
+++ src/app/blogs/(components)/BlogCard.tsx
@@ -1,15 +1,13 @@
-interface blogType{
-    id: number,
-    title: string,
-    body: string,
-    image: string,
-    likes: number,
+interface blogType {
+  id: number
+  title: string
+  body: string
+  image: string
+  likes: number
 }
 
-const BlogCard = ({id,title,body,image,likes}:blogType)=>{
-    <div className="">
-
-    </div>
+const BlogCard = ({ id, title, body, image, likes }: blogType) => {
+  ;<div className=''></div>
 }
 
 export default BlogCard
