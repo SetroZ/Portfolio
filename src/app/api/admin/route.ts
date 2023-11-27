e6c56ae8aecd3ba8ae5319f818f9e1e206f335c3
SetroZ <yousfsbry66@gmail.com>
2023-11-27 15:08:11 +0800

    added admin route

---
+++ src/app/api/admin/route.ts
@@ -0,0 +1,21 @@
+import { cookies } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+  const password = await req.json()
+  try {
+    cookies().set({
+      name: 'password',
+      value: password,
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: true,
+    })
+    return NextResponse.json({ message: true })
+  } catch {
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    )
+  }
+}
